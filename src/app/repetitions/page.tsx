9c337876d69086ab7974b3380c6078e05b386ca8
Amanda Diniz <amandadinizpod@gmail.com>
2023-12-09 19:16:42 -0300

    add repetitions form

---
+++ src/app/repetitions/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { GymnasticForm } from "@/components/GymnasticForm";
+import { Container } from "@chakra-ui/react";
+
+export default function Repetitions() {
+  return (
+    <>
+    <Container pb={10} pt={10} >
+        <GymnasticForm />
+      </Container>
+    </>
+  );
+}
