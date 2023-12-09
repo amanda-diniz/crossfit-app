411b686f8f4237b15030c202d6d38c4753e22d8c
Amanda Diniz <amandadinizpod@gmail.com>
2023-12-09 19:15:34 -0300

    add data

---
+++ src/lib/data.ts
@@ -0,0 +1,19 @@
+const WEIGHTLIFT_EXERCISES = [
+    { name: "deadlift", label: "Deadlift", type: "weightlift" },
+    { name: "front_squat", label: "Front Squat", type: "weightlift" },
+    { name: "back_squat", label: "Back Squat", type: "weightlift" },
+    { name: "power_snatch", label: "Power Snatch", type: "weightlift" },
+    { name: "clean", label: "Clean", type: "weightlift" },
+    { name: "clean_and_jerk", label: "Clean and Jerk", type: "weightlift" },
+    { name: "push_press", label: "Push Press", type: "weightlift" },
+    { name: "bench_press", label: "Bench Press", type: "weightlift" }
+  ];
+  
+  const GYMNASTIC_EXERCISES = [
+    { name: "pull_up", label: "Pull Up", type: "gymnastic" },
+    { name: "push_up", label: "Push Up", type: "gymnastic" },
+    { name: "muscle_up", label: "Muscle Up", type: "gymnastic" },
+  ];
+  
+  export { WEIGHTLIFT_EXERCISES, GYMNASTIC_EXERCISES };
+  
