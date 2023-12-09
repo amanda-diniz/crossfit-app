65d58d6432ad0ffecc6d3db274c65779a976093d
Amanda Diniz <amandadinizpod@gmail.com>
2023-12-09 19:16:13 -0300

    add prs form

---
+++ src/app/prs/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { WeightLiftForm } from "@/components/WeightLiftForm";
+import { Container } from "@chakra-ui/react";
+
+export default function Prs() {
+  return (
+    <>
+    <Container pb={10} pt={10} >
+        <WeightLiftForm />
+      </Container>
+    </>
+  );
+}
